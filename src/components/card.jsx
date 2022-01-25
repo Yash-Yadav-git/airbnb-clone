078d21d36bd33f42311d462d71425d0aec610fc8
Yash Yadav <yashyadavgit@gmail.com>
2022-01-25 23:32:33 +0530

    Navbar added

---
+++ src/components/card.jsx
@@ -0,0 +1,12 @@
+import Reac from "react";
+import ReactDOM from "react-dom";
+
+const card = () => {
+  return (
+    <div>
+      <img src=""></img>
+    </div>
+  );
+};
+
+export default card;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../index.css";
+
+export default function navBar() {
+  return (
+    <nav className="nav-warpper">
+      <img src="./airbnb-logo.png" alt="" className="logo" />
+    </nav>
+  );
+}

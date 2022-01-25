71770776da9b7418d98fda53200bacfd069ae2ee
Yash Yadav <yashyadavgit@gmail.com>
2022-01-26 00:42:50 +0530

    added Card section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import NavBar from "./components/navbar";
 import Card from "./components/card";
+import data from "./data.js";
 
 const App = () => {
+  const Data = data.map((x) => {
+    return <Card key={x.id} item={x} />;
+  });
+
   return (
     <React.StrictMode>
       <NavBar />
-      <Card />
+      {Data}
     </React.StrictMode>
   );
 };
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,22 @@
 import Reac from "react";
 import ReactDOM from "react-dom";
 
-const card = () => {
+const card = (props) => {
   return (
     <div>
-      <img src=""></img>
+      <div className="image-section">
+        <img src={`../Images/${props.item.coverImg}`} alt="" />
+      </div>
+      <div className="review-section">
+        <img src="../Images/star.png" alt="" />
+        <span>{props.item.stats.rating}</span>
+        <span>{props.item.stats.reviewcount}</span>
+        <span>{props.item.stats.location}</span>
+      </div>
+      <div className="title-section">
+        <p>{props.item.title}</p>
+        <p>From ${props.item.price}/person</p>
+      </div>
     </div>
   );
 };
